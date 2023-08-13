52ef268a40826057e670f922b1db812bdd976cf9
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2023-08-13 11:45:33 +0600

    activeNaveLink-added

--- components/Banner/ActiveLink.js
+++ components/Banner/ActiveLink.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Router, withRouter } from 'next/router';
-import style form './ActiveLink.module.css'
+import style from './ActiveLink.module.css'
 
 const ActiveLink = ({router, href, children }) => {
 (function prefetchPages(){
@@ -20,10 +20,10 @@ const handleClick = event =>{
         <div>
             <a href={href}
             onClick={handleClick}
-            style={{background: 'red', color: 'white',
-            background:isCurrentPath ? 'red' : 'yellow'
+        //     style={{background: 'red', color: 'white',
+        //     background:isCurrentPath ? 'red' : 'yellow'
            
-        }}
+        // }}
         className={isCurrentPath ? `${style.activeLink}` : `${style.active}`}
             >
 
